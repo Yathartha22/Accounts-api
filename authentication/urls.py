b9dbcf4e9100f18b20ebe6fb68db4f631a8121a6
Yathartha Joshi <yathartha32@gmail.com>
2017-09-24 23:36:15 +0530

    initial commit

---
+++ authentication/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from .views import AuthRegister, AuthLogin
+from rest_framework_jwt.views import obtain_jwt_token
+
+urlpatterns = [
+	url(r'^register/$', AuthRegister.as_view()),
+	url(r'^login/$', AuthLogin.as_view()),
+	url(r'^api-token-auth/', obtain_jwt_token),
+]
